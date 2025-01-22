332289dcf9fb69cf8c561fe138e7a6d8ec60f4f0
bryanonyc <bryanonyc@yahoo.com>
2025-01-22 15:27:48 -0500

    Update F1StatsApplicationTests.java

--- f1-backend/src/test/java/com/bryano/f1_stats/F1StatsApplicationTests.java
+++ f1-backend/src/test/java/com/bryano/f1_stats/F1StatsApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class F1StatsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
