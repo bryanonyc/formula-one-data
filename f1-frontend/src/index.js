25f2414b8cef651d6d96bac836809a3bfb7fe4b7
bryanonyc <bryanonyc@yahoo.com>
2025-01-22 14:41:07 -0500

    Uninstalling package

--- f1-frontend/src/index.js
+++ f1-frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { App as AntdApp } from "antd"
-import '@ant-design/v5-patch-for-react-19';
+// import '@ant-design/v5-patch-for-react-19';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
