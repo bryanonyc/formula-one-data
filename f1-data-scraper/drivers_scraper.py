d8be40c20892c1ac90731f3c2508436b2eed4cf6
bryanonyc <bryanonyc@yahoo.com>
2025-02-14 17:39:22 -0500

    Convert driver and team scraper into modules so that it can be called externally

--- f1-data-scraper/drivers_scraper.py
+++ f1-data-scraper/drivers_scraper.py
@@ -2,41 +2,42 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-base_url = "https://www.formula1.com"
-all_drivers_url = f"{base_url}/en/drivers"
+def scrape():
+    base_url = "https://www.formula1.com"
+    all_drivers_url = f"{base_url}/en/drivers"
 
-response = requests.get(all_drivers_url).content
-soup = BeautifulSoup(response, 'html.parser')
-anchors = soup.find_all('a', class_='focus-visible:outline-0')
+    response = requests.get(all_drivers_url).content
+    soup = BeautifulSoup(response, 'html.parser')
+    anchors = soup.find_all('a', class_='focus-visible:outline-0')
 
-drivers_data = []
-data_columns = []
+    drivers_data = []
+    data_columns = []
 
-for a in anchors:
-    href = a.get('href')
-    print(f"Scraping {href}")
+    for a in anchors:
+        href = a.get('href')
+        print(f"Scraping {href}")
 
-    driver_response = requests.get(f"{base_url}{href}").content
-    soup = BeautifulSoup(driver_response, 'html.parser')
-    driver_name = soup.find('h1', class_='f1-heading').text
+        driver_response = requests.get(f"{base_url}{href}").content
+        soup = BeautifulSoup(driver_response, 'html.parser')
+        driver_name = soup.find('h1', class_='f1-heading').text
 
-    if len(data_columns) == 0:
-        data_columns.append("Driver")
-        dts = soup.find_all('dt', class_='f1-heading')
-        for dt in dts:
-            data_columns.append(dt.text)
+        if len(data_columns) == 0:
+            data_columns.append("Driver")
+            dts = soup.find_all('dt', class_='f1-heading')
+            for dt in dts:
+                data_columns.append(dt.text)
 
-    driver_data = []
-    driver_data.append(driver_name)
+        driver_data = []
+        driver_data.append(driver_name)
 
-    dds = soup.find_all('dd', class_='f1-text')
-    for dd in dds:
-        driver_data.append(dd.text)
+        dds = soup.find_all('dd', class_='f1-text')
+        for dd in dds:
+            driver_data.append(dd.text)
 
-    drivers_data.append(driver_data)
+        drivers_data.append(driver_data)
 
-# Construct a pandas DataFrame
-data_frames = pd.DataFrame(drivers_data, columns=data_columns)
+    # Construct a pandas DataFrame
+    data_frames = pd.DataFrame(drivers_data, columns=data_columns)
 
-# Output Data Frame to CSV without the deffault index
-data_frames.to_csv('driver_stats.csv', index=False)
+    # Output Data Frame to CSV without the deffault index
+    data_frames.to_csv('driver_stats.csv', index=False)
--- f1-data-scraper/teams_scraper.py
+++ f1-data-scraper/teams_scraper.py
@@ -2,34 +2,35 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-base_url = "https://www.formula1.com"
-all_teams_url = f"{base_url}/en/teams"
+def scrape():
+    base_url = "https://www.formula1.com"
+    all_teams_url = f"{base_url}/en/teams"
 
-response = requests.get(all_teams_url).content
-soup = BeautifulSoup(response, 'html.parser')
-anchors = soup.find_all('a', class_='focus-visible:outline-0')
+    response = requests.get(all_teams_url).content
+    soup = BeautifulSoup(response, 'html.parser')
+    anchors = soup.find_all('a', class_='focus-visible:outline-0')
 
-teams_data = []
-column_headers = []
+    teams_data = []
+    column_headers = []
 
-for a in anchors:
-    href = a.get('href')
-    print(f"Scraping {href}")
+    for a in anchors:
+        href = a.get('href')
+        print(f"Scraping {href}")
 
-    team_response = requests.get(f"{base_url}{href}").content
-    soup = BeautifulSoup(team_response, 'html.parser')
+        team_response = requests.get(f"{base_url}{href}").content
+        soup = BeautifulSoup(team_response, 'html.parser')
 
-    if len(column_headers) == 0:
-        dts = soup.find_all('dt', class_='f1-heading')
-        for dt in dts:
-            column_headers.append(dt.text)
+        if len(column_headers) == 0:
+            dts = soup.find_all('dt', class_='f1-heading')
+            for dt in dts:
+                column_headers.append(dt.text)
 
-    team_data = []
-    dds = soup.find_all('dd', class_='f1-text')
-    for dd in dds:
-        team_data.append(dd.text)
+        team_data = []
+        dds = soup.find_all('dd', class_='f1-text')
+        for dd in dds:
+            team_data.append(dd.text)
 
-    teams_data.append(team_data)
+        teams_data.append(team_data)
 
-df = pd.DataFrame(teams_data, columns=column_headers)
-df.to_csv('team_stats.csv', index=False)
+    df = pd.DataFrame(teams_data, columns=column_headers)
+    df.to_csv('team_stats.csv', index=False)
