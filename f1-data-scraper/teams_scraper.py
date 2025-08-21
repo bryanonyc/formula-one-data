52850b9a9b902c41cb619df3df470baea0c2da06
bryanonyc <bryanonyc@yahoo.com>
2025-08-21 14:56:30 -0400

    Fix broken scrapers due to website changes

--- f1-data-scraper/teams_scraper.py
+++ f1-data-scraper/teams_scraper.py
@@ -8,29 +8,71 @@ def scrape():
 
     response = requests.get(all_teams_url).content
     soup = BeautifulSoup(response, 'html.parser')
-    anchors = soup.find_all('a', class_='focus-visible:outline-0')
+    anchors = soup.find_all(attrs={'data-f1rd-a7s-click': 'team_card_click'})
 
-    teams_data = []
-    column_headers = []
+    column_headers = ['full_team_name', 'base', 'team_chief', 'technical_chief',
+                      'chassis', 'power_unit', 'first_team_entry',
+                      'world_championships', 'highest_race_finish',
+                      'pole_positions']
+    data = []
 
     for a in anchors:
         href = a.get('href')
+
+        if not href:
+            continue
+
         print(f"Scraping {href}")
 
         team_response = requests.get(f"{base_url}{href}").content
         soup = BeautifulSoup(team_response, 'html.parser')
 
-        if len(column_headers) == 0:
-            dts = soup.find_all('dt', class_='f1-heading')
-            for dt in dts:
-                column_headers.append(dt.text)
-
         team_data = []
-        dds = soup.find_all('dd', class_='f1-text')
-        for dd in dds:
-            team_data.append(dd.text)
+        team_data.append(_get_team_name(soup))
+        team_data.append(_get_base(soup))
+        team_data.append(_get_team_chief(soup))
+        team_data.append(_get_technical_chief(soup))
+        team_data.append(_get_chassis(soup))
+        team_data.append(_get_power_unit(soup))
+        team_data.append(_get_first_team_entry(soup))
+        team_data.append(_get_world_championships(soup))
+        team_data.append(_get_highest_race_finish(soup))
+        team_data.append(_get_pole_positions(soup))
 
-        teams_data.append(team_data)
+        data.append(team_data)
 
-    df = pd.DataFrame(teams_data, columns=column_headers)
-    df.to_csv('team_stats.csv', index=False)
+    data_frames = pd.DataFrame(data, columns=column_headers)
+    data_frames.to_csv('team_stats.csv', index=False)
+
+def _get_team_name(soup):
+    return soup.find('dt', string='Full Team Name').find_next_sibling('dd').text
+
+def _get_base(soup):
+    return soup.find('dt', string='Base').find_next_sibling('dd').text
+
+def _get_team_chief(soup):
+    return soup.find('dt', string='Team Chief').find_next_sibling('dd').text
+
+def _get_technical_chief(soup):
+    return soup.find('dt', string='Technical Chief').find_next_sibling('dd').text
+
+def _get_chassis(soup):
+    return soup.find('dt', string='Chassis').find_next_sibling('dd').text
+
+def _get_power_unit(soup):
+    return soup.find('dt', string='Power Unit').find_next_sibling('dd').text
+
+def _get_first_team_entry(soup):
+    return soup.find('dt', string='First Team Entry').find_next_sibling('dd').text
+
+def _get_world_championships(soup):
+    return soup.find('dt', string='World Championships').find_next_sibling('dd').text
+
+def _get_highest_race_finish(soup):
+    return soup.find('dt', string='Highest Race Finish').find_next_sibling('dd').text
+
+def _get_pole_positions(soup):
+    return soup.find('dt', string='Pole Positions').find_next_sibling('dd').text
+
+if __name__ == "__main__":
+    scrape()
