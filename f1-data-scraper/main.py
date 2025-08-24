c3f746451e642b797516e67b08929f250801d516
bryanonyc <bryanonyc@yahoo.com>
2025-08-24 09:10:35 -0400

    Adding routes to scrape driver archive and team archive data

--- f1-data-scraper/main.py
+++ f1-data-scraper/main.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import drivers_archive_scraper
 import drivers_scraper
+import teams_archive_scraper
 import teams_scraper
 from flask import Flask, jsonify
 
@@ -20,6 +22,17 @@ def current_drivers():
     # Return JSON response using jsonify
     return jsonify(data)
 
+@app.route("/api/drivers/archive")
+def archive_drivers():
+    drivers_archive_scraper.scrape()
+
+    df = pd.read_csv("drivers_archive.csv")
+
+    data = df.to_dict(orient="records")
+
+    # Return JSON response using jsonify
+    return jsonify(data)
+
 @app.route("/api/teams/current")
 def current_teams():
     teams_scraper.scrape()
@@ -31,6 +44,16 @@ def current_teams():
     # Return JSON response using jsonify
     return jsonify(data)
 
+@app.route("/api/teams/archive")
+def archive_teams():
+    teams_archive_scraper.scrape()
+
+    df = pd.read_csv("teams_archive.csv")
+
+    data = df.to_dict(orient="records")
+
+    # Return JSON response using jsonify
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run()
