6986801965a889cb1a6acb4158a57c34f9a0b01b
bryanonyc <bryanonyc@yahoo.com>
2025-08-24 11:10:48 -0400

    Add endpoints to scrape driver archive and team archive data

--- f1-backend/src/main/java/http/DataScraperHttpClient.java
+++ f1-backend/src/main/java/http/DataScraperHttpClient.java
@@ -1,7 +1,9 @@
 package http;
 
 import com.bryano.f1_stats.driver.ScrapedDriver;
+import com.bryano.f1_stats.driver_archive.ScrapedDriverArchive;
 import com.bryano.f1_stats.team.ScrapedTeam;
+import com.bryano.f1_stats.team_archive.ScrapedTeamArchive;
 import org.springframework.web.service.annotation.GetExchange;
 
 import java.util.List;
@@ -10,6 +12,12 @@ public interface DataScraperHttpClient {
     @GetExchange("/api/drivers/current")
     List<ScrapedDriver> getDrivers();
 
+    @GetExchange("/api/drivers/archive")
+    List<ScrapedDriverArchive> scrapeDriverArchive();
+
     @GetExchange("/api/teams/current")
     List<ScrapedTeam> getTeams();
+
+    @GetExchange("/api/teams/archive")
+    List<ScrapedTeamArchive> scrapeTeamArchive();
 }
