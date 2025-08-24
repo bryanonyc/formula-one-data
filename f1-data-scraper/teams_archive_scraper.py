fe3ed4f8c3b11736dca134d5f37d5dcd6f30f988
bryanonyc <bryanonyc@yahoo.com>
2025-08-24 09:10:04 -0400

    Convert scraper into modules so that it can be called externally

--- f1-data-scraper/teams_archive_scraper.py
+++ f1-data-scraper/teams_archive_scraper.py
@@ -3,29 +3,33 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-base_url = "https://www.formula1.com/en/results"
+def scrape():
+    base_url = "https://www.formula1.com/en/results"
 
-start_year = 1958
-end_year = date.today().year
+    start_year = 1958
+    end_year = date.today().year
 
-column_headers = ['year', 'position', 'team', 'points']
-archive_data = []
+    column_headers = ['year', 'position', 'team', 'points']
+    archive_data = []
 
-for year in range(start_year, end_year):
-    archive_url = f"{base_url}/{year}/team"
-    print(f"Scraping {base_url}/{year}/team")
+    for year in range(start_year, end_year):
+        archive_url = f"{base_url}/{year}/team"
+        print(f"Scraping {base_url}/{year}/team")
 
-    response = requests.get(archive_url).content
-    soup = BeautifulSoup(response, 'html.parser')
+        response = requests.get(archive_url).content
+        soup = BeautifulSoup(response, 'html.parser')
 
-    rows = soup.find('tbody').find_all('tr')
-    for row in rows:
-        data = []
-        data.append(year)
-        ps = row.find_all('p')
-        for p in ps:
-            data.append(p.text)
-        archive_data.append(data)
+        rows = soup.find('tbody').find_all('tr')
+        for row in rows:
+            data = []
+            data.append(year)
+            ps = row.find_all('p')
+            for p in ps:
+                data.append(p.text)
+            archive_data.append(data)
 
-df = pd.DataFrame(archive_data, columns=column_headers)
-df.to_csv('teams_archive.csv', index=False)
+    df = pd.DataFrame(archive_data, columns=column_headers)
+    df.to_csv('teams_archive.csv', index=False)
+
+if __name__ == "__main__":
+    scrape()
