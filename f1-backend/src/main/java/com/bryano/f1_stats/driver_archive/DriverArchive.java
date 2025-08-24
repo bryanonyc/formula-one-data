5a2f0abd1d75e5ff896fb412a91f3520a938e09f
bryanonyc <bryanonyc@yahoo.com>
2025-08-24 11:08:02 -0400

    Add @Table annotation

--- f1-backend/src/main/java/com/bryano/f1_stats/driver_archive/DriverArchive.java
+++ f1-backend/src/main/java/com/bryano/f1_stats/driver_archive/DriverArchive.java
@@ -1,13 +1,11 @@
 package com.bryano.f1_stats.driver_archive;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.util.Objects;
 
 @Entity
+@Table(name = "driver_archive")
 public class DriverArchive {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- f1-backend/src/main/java/com/bryano/f1_stats/team_archive/TeamArchive.java
+++ f1-backend/src/main/java/com/bryano/f1_stats/team_archive/TeamArchive.java
@@ -1,13 +1,11 @@
 package com.bryano.f1_stats.team_archive;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.util.Objects;
 
 @Entity
+@Table(name = "team_archive")
 public class TeamArchive {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
