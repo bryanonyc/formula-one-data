8f7940e07b21205c92f3191121335f0674a6bd70
bryanonyc <bryanonyc@yahoo.com>
2025-08-25 09:48:17 -0400

    Add docker files for running UI in a container

---
+++ f1-frontend/docker/docker-create.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Create and start project containers, only if containers do not exist
+if [[ $(docker ps -a) != *"f1-ui"* ]]; then
+  printf "No container exist, attempting full compose.\n\n"
+  docker-compose up --build -d
+  sleep 10
+else
+  printf "Container is stopped.\nAttempting start...\n\n"
+  sh ./docker-start.sh
+fi
